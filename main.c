3f479c6bf0297cbded40ab86e874bed5e64885dc
endless.sine@gmail.com <endless.sine@gmail.com>
2023-01-11 18:01:06 +0200

    added help command

--- main.c
+++ main.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "linked_list.h"
 #include "main.h"
-
 
 int main(int argc, char *argv[]) {
   struct node *list = list_init_from_arg(argv[1]);
@@ -14,7 +13,8 @@ int main(int argc, char *argv[]) {
 
 struct node *list_init_from_arg(char *init) {
   if (init[0] != 'i' || strlen(init) < 2) {
-    printf("List must start with valid initializer!");
+    printf("List must start with valid initializer! \n");
+    help();
     return NULL;
   }
   int initializer = atoi(init + 1);
@@ -33,7 +33,7 @@ int parse_args(int argc, char *argv[], struct node *list) {
       printf("Appending Node \n");
       int new_node;
       // sscanf(argv[i], "a%d", &new_node); // old bad implementation
-      new_node = atoi(argv[i]+1);
+      new_node = atoi(argv[i] + 1);
       add_node(new_node, list);
       break;
     case 'd':
@@ -44,9 +44,19 @@ int parse_args(int argc, char *argv[], struct node *list) {
       printf("Prettyprinting your list! \n");
       pretty_print(list);
       break;
+    case 'h':
+      help();
+      break;
+
     default:
       printf("%s, Command not recognised!\n", argv[i]);
     }
   }
   return 0;
+}
+
+void help() {
+  printf("iX - initialize list with X as the contents\naX - append node with X "
+         "as the contents\nd - delete last node\n"
+         "p - prettyprint the linked list\nh - help \n");
 }
--- main.h
+++ main.h
@@ -1,2 +1,3 @@
+void help();
 int parse_args(int argc, char *argv[], struct node *list);
 struct node *list_init_from_arg(char *init);

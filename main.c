aed912fc258932a892129e61bd76bcbfb2a979e3
endless.sine@gmail.com <endless.sine@gmail.com>
2023-01-11 13:02:49 +0200

    parsinggg

--- main.c
+++ main.c
@@ -1,16 +1,49 @@
+#include <stdio.h>
+#include <string.h>
 #include "linked_list.h"
+#include "main.h"
 
 
-// todo: structure DSL
+int main(int argc, char *argv[]) {
+  struct node *list = list_init_from_arg(argv[1]);
+  parse_args(argc, argv, list);
+  return 0;
+}
 
-int main(int argc, char *argv[])
-{
-        struct node *list = init_list(1);
-        add_node(7, list);
-        add_node(3, list);
-        add_node(8, list);
-        add_node(3, list);
-        delete_node(list);
-        pretty_print(list);
-        return 0;
+struct node *list_init_from_arg(char *init) {
+  if (init[0] != 'i' || strlen(init) < 2) {
+    printf("List must start with valid initializer!");
+    return NULL;
+  }
+  int initializer = init[1] - '0';
+  printf("List initialized with %i\n", initializer);
+  return init_list(initializer);
+}
+
+int parse_args(int argc, char *argv[], struct node *list) {
+  for (int i = 2; i < argc; i++) {
+    switch (argv[i][0]) {
+    case 'a':
+      if (strlen(argv[i]) < 2) {
+        printf("Invalid append command, appending failed! \n");
+        break;
+      }
+      printf("Appending Node \n");
+      int new_node;
+      sscanf(argv[i], "a%d", &new_node);
+      add_node(new_node, list);
+      break;
+    case 'd':
+      printf("Deleting Node\n");
+      delete_node(list);
+      break;
+    case 'p':
+      printf("Prettyprinting your list! \n");
+      pretty_print(list);
+      break;
+    default:
+      printf("%s, Command not recognised!\n", argv[i]);
+    }
+  }
+  return 0;
 }
---
+++ main.h
@@ -0,0 +1,2 @@
+int parse_args(int argc, char *argv[], struct node *list);
+struct node *list_init_from_arg(char *init);
